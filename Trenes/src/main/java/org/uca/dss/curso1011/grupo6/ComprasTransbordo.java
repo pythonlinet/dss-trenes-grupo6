136ff634c75c28f34b0b8944138f0d8210ee1991
joselar87 <joselar87@85ec396f-0c6f-b9ca-7643-ba557e8dfac9>
2011-05-18 15:27:59 +0000

    Clase nueva ComprasTransbordo. Añadida funciones a Transbordo.

---
+++ Trenes/src/main/java/org/uca/dss/curso1011/grupo6/ComprasTransbordo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.uca.dss.curso1011.grupo6;
+
+/**
+ *
+ * @author Jose Luis
+ */
+public class ComprasTransbordo {
+
+}
