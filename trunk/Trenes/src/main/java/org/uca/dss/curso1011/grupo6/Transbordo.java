467c21dc946c094938d862fd66a78ccb9332df59
joselar87 <joselar87@85ec396f-0c6f-b9ca-7643-ba557e8dfac9>
2011-05-18 15:33:51 +0000

    Añadida función getHorariosEntre con errores.

--- trunk/Trenes/src/main/java/org/uca/dss/curso1011/grupo6/Transbordo.java
+++ trunk/Trenes/src/main/java/org/uca/dss/curso1011/grupo6/Transbordo.java
@@ -5,11 +5,16 @@
 
 package org.uca.dss.curso1011.grupo6;
 
+import com.db4o.ObjectContainer;
+import com.db4o.query.Predicate;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.uca.dss.curso1011.grupo6.interfazExtendido.InterfazListados;
 import org.uca.dss.curso1011.grupo6.interfazExtendido.Itinerario;
+import org.uca.dss.trenes.basededatos.DBUtils;
 
 /**
  *
@@ -40,8 +45,40 @@ public class Transbordo implements InterfazListados {
 
     public List<Itinerario> getHorariosEntre(String origen, String destino, LocalDate fechaSalida, LocalTime horaSalida, LocalTime horaLlegada) {
 
-        List<Itinerario> itinerarios = null;
-        return itinerarios;
+        List<Itinerario> itinerariosDisponibles = new ArrayList();
+
+        Iterator i = trayectos.iterator();
+
+        List<Reserva> reservasValidas;
+        reservasValidas = new ArrayList();
+
+        //ComprobarExcepcion(fecha,origen,destino); Importante!!!
+
+        ObjectContainer db = DBUtils.getDb();
+
+        List <Reserva> reservas = db.query(new Predicate <Reserva>() {
+            public boolean match ( Reserva reserva) {
+                return true;
+            }
+        }) ;
+         while (i.hasNext())
+         {
+           Trayecto trayecto = (Trayecto)i.next();
+           if(trayecto.getCiudadOrigen().equals(origen) &&
+                   trayecto.getCiudadDestino().equals(destino))
+           {
+               //reservasValidas = ObtenerReservas(trayecto,fecha);
+
+               if(trayecto.getTren().getPlazas() - reservasValidas.size() > 0)
+               {
+
+                   LocalTime hora = trayecto.getHorario().getSalida();
+                   //horariosDisponibles.add(hora);
+               }
+           }
+        }
+
+        return itinerariosDisponibles;
     }
 
     public List<Itinerario> getHorarios(String origen, String destino, LocalDate fechaSalida) {
