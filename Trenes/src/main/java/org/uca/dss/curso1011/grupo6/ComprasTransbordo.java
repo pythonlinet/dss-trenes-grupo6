78de9a8640d86adf92b879c2b7fdd932cce89a08
joselar87 <joselar87@85ec396f-0c6f-b9ca-7643-ba557e8dfac9>
2011-05-21 08:31:00 +0000

    Modificacion getHorarios

--- Trenes/src/main/java/org/uca/dss/curso1011/grupo6/ComprasTransbordo.java
+++ Trenes/src/main/java/org/uca/dss/curso1011/grupo6/ComprasTransbordo.java
@@ -5,10 +5,32 @@
 
 package org.uca.dss.curso1011.grupo6;
 
+import java.util.List;
+import org.joda.time.LocalDate;
+import org.uca.dss.curso1011.grupo6.interfazExtendido.InterfazCompras;
+import org.uca.dss.curso1011.grupo6.interfazExtendido.Itinerario;
+import org.uca.dss.curso1011.grupo6.interfazExtendido.ReservaTrayecto;
+
 /**
  *
  * @author Jose Luis
  */
-public class ComprasTransbordo {
+public class ComprasTransbordo implements InterfazCompras{
+
+    public List<ReservaTrayecto> reservaAsiento(Itinerario itinerario, LocalDate fecha) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int asientosLibres(LocalDate fecha, Itinerario itinerario) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void cancelaReserva(ReservaTrayecto reserva) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void cancelaReserva(List<ReservaTrayecto> reservas) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
