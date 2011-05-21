b283a018c8d350fa28976d291fc290e972454c45
joselar87 <joselar87@85ec396f-0c6f-b9ca-7643-ba557e8dfac9>
2011-05-21 08:49:45 +0000

    Modificacion getHorarios

---
+++ trunk/Trenes/src/main/java/org/uca/dss/curso1011/grupo6/ItinerarioImplementacionInterfaz.java
@@ -0,0 +1,117 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.uca.dss.curso1011.grupo6;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import org.uca.dss.curso1011.grupo6.interfazExtendido.InformacionTrayecto;
+import org.uca.dss.curso1011.grupo6.interfazExtendido.Itinerario;
+
+/**
+ *
+ * @author Jose Luis
+ */
+public class ItinerarioImplementacionInterfaz implements Itinerario{
+
+    public double getPrecio() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Iterator<InformacionTrayecto> iterator() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean add(InformacionTrayecto e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean addAll(Collection<? extends InformacionTrayecto> c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean addAll(int index, Collection<? extends InformacionTrayecto> c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public InformacionTrayecto get(int index) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public InformacionTrayecto set(int index, InformacionTrayecto element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void add(int index, InformacionTrayecto element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public InformacionTrayecto remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ListIterator<InformacionTrayecto> listIterator() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ListIterator<InformacionTrayecto> listIterator(int index) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<InformacionTrayecto> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
