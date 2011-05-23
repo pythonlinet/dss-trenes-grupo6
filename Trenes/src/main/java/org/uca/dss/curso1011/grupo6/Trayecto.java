09964f9949ebc9961f0078305efb40ed58d40dad
elmaslargo87 <elmaslargo87@85ec396f-0c6f-b9ca-7643-ba557e8dfac9>
2011-05-23 16:48:29 +0000

    Actualizacion comentarios de la clase Trayecto

--- Trenes/src/main/java/org/uca/dss/curso1011/grupo6/Trayecto.java
+++ Trenes/src/main/java/org/uca/dss/curso1011/grupo6/Trayecto.java
@@ -6,7 +6,10 @@
 package org.uca.dss.curso1011.grupo6;
 
 /**
- * Clase que contiene los trayectos disponibles que se pueden realizar
+ * Clase que contiene los trayectos disponibles que se pueden realizar entre las
+ * diferentes ciudades y los horarios disponibles que tenemos de ellos. También 
+ * tenemos el numero de tramos del que se compone el trayecto. El tren con el que
+ * se realiza el trayecto también lo guardamos
  * @author Jose Luis Aparicio Rodriguez
  * @author Daniel Ruiz Camacho
  * @author Juan Carlos Ríos Legupín
@@ -20,10 +23,10 @@ public class Trayecto {
     private Horario horario;    
 
     /**
-     * 
-     * @param CiudadOrigen
-     * @param CiudadDestino
-     * @param NumeroTramos
+     * Contructor de la clase trayecto
+     * @param ciudadOrigen del trayecto
+     * @param ciudadDestino del trayecto
+     * @param numeroTramos del trayecto
      */
     public Trayecto(String ciudadOrigen, String ciudadDestino, int numeroTramos)
     {
@@ -32,80 +35,90 @@ public class Trayecto {
         this.numeroTramos = numeroTramos;
     }
     /**
-     *
-     * @param trenTrayecto
-     * @return
+     * Calcula el precio del trayecto por el numero de tramos
+     * @param trenTrayecto, tren del trayecto
+     * @return precio del trayecto con el tren con el que lo realizamos
      */
 
-    public double CalcularPrecio(Tren trenTrayecto){
+    public double calcularPrecio(Tren trenTrayecto){
         return trenTrayecto.getPrecio() * getNumeroTramos();
     }
 
     /**
-     * @return the CiudadOrigen
+     * Metodo que se encarga de obtener la ciudad de origen del trayecto
+     * @return CiudadOrigen del trayecto
      */
     public String getCiudadOrigen() {
         return ciudadOrigen;
     }
 
     /**
-     * @param CiudadOrigen the CiudadOrigen to set
+     * Metodo que se encarga de introducir la ciudad origen en el trayecto
+     * @param CiudadOrigen del trayecto
      */
     public void setCiudadOrigen(String ciudadOrigen) {
         this.ciudadOrigen = ciudadOrigen;
     }
 
     /**
-     * @return the CiudadDestino
+     * Metodo que obtiene la ciudad de destino del trayecto
+     * @return CiudadDestino del trayecto
      */
     public String getCiudadDestino() {
         return ciudadDestino;
     }
 
     /**
-     * @param CiudadDestino the CiudadDestino to set
+     * Metodo que se encarga de insertar la ciudad de destino en el trayecto
+     * @param CiudadDestino del trayecto
      */
     public void setCiudadDestino(String ciudadDestino) {
         this.ciudadDestino = ciudadDestino;
     }
 
     /**
-     * @return the NumeroTramos
+     * Metodo que obtiene el numero de tramos de un trayecto
+     * @return numeroTramos del trayecto
      */
     public int getNumeroTramos() {
         return numeroTramos;
     }
 
     /**
-     * @param NumeroTramos the NumeroTramos to set
+     * Metodo que inserta el numero de tramos en el trayecto
+     * @param NumeroTramos del trayecto
      */
     public void setNumeroTramos(int numeroTramos) {
         this.numeroTramos = numeroTramos;
     }
 
     /**
-     * @return the tren
+     * Metodo que obtiene el tren en el que se realiza el trayecto
+     * @return tren del trayecto
      */
     public Tren getTren() {
         return tren;
     }
 
     /**
-     * @param tren the tren to set
+     * Metodo que se encarga de insertar el metodo en el trayecto
+     * @param tren del trayecto
      */
     public void setTren(Tren tren) {
         this.tren = tren;
     }
 
     /**
-     * @return the horario
+     * Metodo que se encarga de obtener el horario del trayecto
+     * @return horario del trayecto
      */
     public Horario getHorario() {
         return horario;
     }
 
     /**
-     * @param horario the horario to set
+     * Metodo que se encarga de insertar el horario en el trayecto
+     * @param horario del trayecto
      */
     public void setHorario(Horario horario) {
         this.horario = horario;
